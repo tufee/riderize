31769c12cb8f23ad85b1e5cd1eb0179c5d2af2b3
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-05-11 14:46:48 -0300

    corrige migration

--- prisma/migrations/20230511172221_user/migration.sql
+++ prisma/migrations/20230511172221_user/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "passsword" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
