adb0889d75af029ea9b28dbff746b27c988d8b5e
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-08-24 04:54:30 -0300

    adiciona interface IUserLogin e validações

--- src/infra/api/graphql/input/login-input.ts
+++ src/infra/api/graphql/input/login-input.ts
@@ -1,10 +1,15 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
+import { IUserLogin } from '../../../../domain/interfaces/User';
 
 @InputType()
-export class LoginInput {
+export class LoginInput implements IUserLogin {
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
