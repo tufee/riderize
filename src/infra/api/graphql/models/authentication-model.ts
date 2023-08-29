39fa54ade42d19c39ba869820176e76c5e92822e
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-08-29 09:06:54 -0300

    altera nome de model para type

--- src/infra/api/graphql/models/authentication-model.ts
+++
@@ -1,8 +0,0 @@
-import { Field, ObjectType } from 'type-graphql';
-import { IToken } from '../../../interfaces/authentication/authentication';
-
-@ObjectType()
-export class AuthenticationToken implements IToken {
-  @Field()
-  token: string;
-}
