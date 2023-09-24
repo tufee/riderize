e71468c6f27f072548aedd5ad3db57cbcc22b471
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-24 01:49:06 -0300

    refatora o payload de envio

--- src/infra/api/repositores/prisma/ride-repository.ts
+++ src/infra/api/repositores/prisma/ride-repository.ts
@@ -6,18 +6,9 @@ import { prisma } from './prisma-client';
 @Service()
 export class RideRepository implements IRideRepository {
 
-  async save(ride: CreateRideInput): Promise<any> {
+  async save(data: CreateRideInput): Promise<any> {
     return await prisma.ride.create({
-      data: {
-        user_id: ride.user_id,
-        name: ride.name,
-        start_date: ride.start_date,
-        start_date_registration: ride.start_date_registration,
-        end_date_registration: ride.end_date_registration,
-        additional_information: ride.additional_information,
-        start_place: ride.start_place,
-        participants_limit: ride.participants_limit
-      }
+      data
     });
   }
 }
